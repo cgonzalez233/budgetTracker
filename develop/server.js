870757e642b3131dfd8a97674ca21cfb8bf37ee2
Christian Gonzalez <cgonzalez233@yahoo.com>
2021-01-29 12:09:23 -0500

    finished with service worker and works offline

--- develop/server.js
+++ develop/server.js
@@ -12,12 +12,12 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`http://localhost:${PORT}`);
 });
